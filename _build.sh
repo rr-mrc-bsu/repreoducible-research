a3c44e027108991dd3ef1dcfd71c6cbf627c8115
Kevin <kevinkunzmann@gmx.net>
2019-01-24 18:08:48 +0000

    changed build script to automatically build all formats

--- _build.sh
+++ _build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-Rscript -e 'bookdown::render_book("index.Rmd")'
-rm -rf _bookdown_files
+Rscript -e 'bookdown::render_book("index.Rmd", output_format = "all")'
+rm -rf _bookdown_files/
