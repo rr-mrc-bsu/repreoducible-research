2ca5ff288585fba56fc832f056430e16587d7e8b
magnusmunch <magnus_munch@hotmail.com>
2019-02-09 00:01:23 +0100

    added workflow diagram

---
+++ Code/diagrams.R
@@ -0,0 +1,29 @@
+# ---- workflow-diagram ----
+openplotmat()
+par(mar=c(1, 1, 1, 1))
+openplotmat()
+elpos <- coordinates(c(2, 1, 2, 2))
+fromto <- matrix(c(1, 3, 2, 3, 3, 4, 3, 5, 4, 6, 4, 7, 5, 6, 5, 7), ncol=2,
+                 byrow=TRUE)
+nr <- nrow(fromto)
+arrpos <- matrix(ncol=2, nrow=nr)
+for(i in 1:nr) {
+    arrpos[i, ] <- straightarrow(to=elpos[fromto[i, 2], ],
+                                 from=elpos[fromto[i, 1], ], lwd=2, arr.pos=0.6,
+                                 arr.length=0.5)
+}
+textrect(elpos[1, ], 0.15, 0.05, lab="functions.R", box.col="gray",
+         shadow.col="grey", shadow.size=0, cex=1.5)
+textrect(elpos[2, ], 0.15, 0.05, lab="vignette.Rnw", box.col="gray",
+         shadow.col="grey", shadow.size=0, cex=1.5)
+textellipse(elpos[3, ], 0.2, 0.1, lab="r-package.tar.gz", box.col="orange",
+            shadow.col="darkgreen", shadow.size=0, cex=1.5)
+textrect(elpos[4, ], 0.15, 0.05, lab="analysis.R", box.col="gray",
+         shadow.col="darkblue", shadow.size=0, cex=1.5)
+textrect(elpos[5, ], 0.15, 0.05, lab="simulation.R", box.col="gray",
+         shadow.col="darkblue", shadow.size=0, cex=1.5)
+textellipse(elpos[6, ], 0.2, 0.1, lab="manuscript.Rnw", box.col="orange",
+            shadow.col="red", shadow.size=0, cex=1.5)
+textellipse(elpos[7, ], 0.2, 0.1, lab="presentation.Rnw", box.col="orange",
+            shadow.col="red", shadow.size=0, cex=1.5)
+
