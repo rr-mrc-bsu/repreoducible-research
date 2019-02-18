d256551ef4e2621ce77c4b4b793cac5eb2c5ee78
magnusmunch <magnus_munch@hotmail.com>
2019-02-18 21:33:38 +0100

    changed text

--- Code/diagrams.R
+++ Code/diagrams.R
@@ -2,9 +2,9 @@
 openplotmat()
 par(mar=c(1, 1, 1, 1))
 openplotmat()
-elpos <- coordinates(c(2, 1, 2, 2))
-fromto <- matrix(c(1, 3, 2, 3, 3, 4, 3, 5, 4, 6, 4, 7, 5, 6, 5, 7), ncol=2,
-                 byrow=TRUE)
+elpos <- coordinates(c(2, 1, 2, 2, 2, 1))
+fromto <- matrix(c(1, 3, 2, 3, 3, 4, 3, 5, 4, 6, 5, 7, 6, 8, 6, 9, 7, 8, 7, 9,
+                   10, 8, 10, 9), ncol=2, byrow=TRUE)
 nr <- nrow(fromto)
 arrpos <- matrix(ncol=2, nrow=nr)
 for(i in 1:nr) {
@@ -14,7 +14,7 @@ for(i in 1:nr) {
 }
 textrect(elpos[1, ], 0.15, 0.05, lab="functions.R", box.col="gray",
          shadow.col="grey", shadow.size=0, cex=1.5)
-textrect(elpos[2, ], 0.15, 0.05, lab="vignette.Rnw", box.col="gray",
+textrect(elpos[2, ], 0.15, 0.05, lab="DESCRIPTION", box.col="gray",
          shadow.col="grey", shadow.size=0, cex=1.5)
 textellipse(elpos[3, ], 0.2, 0.1, lab="r-package.tar.gz", box.col="orange",
             shadow.col="darkgreen", shadow.size=0, cex=1.5)
@@ -22,8 +22,14 @@ textrect(elpos[4, ], 0.15, 0.05, lab="analysis.R", box.col="gray",
          shadow.col="darkblue", shadow.size=0, cex=1.5)
 textrect(elpos[5, ], 0.15, 0.05, lab="simulation.R", box.col="gray",
          shadow.col="darkblue", shadow.size=0, cex=1.5)
-textellipse(elpos[6, ], 0.2, 0.1, lab="manuscript.Rnw", box.col="orange",
+textrect(elpos[6, ], 0.15, 0.05, lab="results1.csv", box.col="gray",
+         shadow.col="darkblue", shadow.size=0, cex=1.5)
+textrect(elpos[7, ], 0.15, 0.05, lab="results2.csv", box.col="gray",
+         shadow.col="darkblue", shadow.size=0, cex=1.5)
+textellipse(elpos[8, ], 0.2, 0.1, lab="manuscript.Rnw", box.col="orange",
             shadow.col="red", shadow.size=0, cex=1.5)
-textellipse(elpos[7, ], 0.2, 0.1, lab="presentation.Rnw", box.col="orange",
+textellipse(elpos[9, ], 0.2, 0.1, lab="presentation.Rnw", box.col="orange",
             shadow.col="red", shadow.size=0, cex=1.5)
+textrect(elpos[10, ], 0.15, 0.05, lab="refs.bib", box.col="gray",
+         shadow.col="darkblue", shadow.size=0, cex=1.5)
 
